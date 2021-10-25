aead8e2f6898977acf7ee3980a98add42167f2c3
Regalk <jsalazarcaviedes30@gmail.com>
2021-10-25 10:14:58 -0500

    Adding dependencies

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
+#include<vector>
+#include<string>
+#include<ctime>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_ttf.h>
 
+
+using namespace std;
 
 int main(){
-    std::cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
     return 0;
 }
