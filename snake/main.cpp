9c6a8e74f29ad0b9bb9e334be8fc1a4691461f93
Regalk <jsalazarcaviedes30@gmail.com>
2021-10-27 10:19:11 -0500

    Now The snake works

--- snake/main.cpp
+++ snake/main.cpp
@@ -71,7 +71,7 @@ pair<int, int> foodSpawn(vector<int> tailX, vector<int> tailY, int playerX, int 
     bool valid = false;
     int x = 0;
     int y = 0;
-    scrand(time(0));
+    srand(time(0));
     x = scale * (rand() % wScale);
     y = scale * (rand() % wScale);
     valid = true;
@@ -79,7 +79,7 @@ pair<int, int> foodSpawn(vector<int> tailX, vector<int> tailY, int playerX, int 
 
     for(int i = 0; i < tailLength; i++){
 
-        if((x == tailX[1] && tailY[1]) || (x == playerX && playerY)){
+        if((x == tailX[1] && y == tailY[1]) || (x == playerX && y == playerY)){
             valid = false;
         }
 
@@ -87,7 +87,7 @@ pair<int, int> foodSpawn(vector<int> tailX, vector<int> tailY, int playerX, int 
 
     if (!valid){
         pair<int, int> foodLoc;
-        foodLoc = make_pair(-100, -100):
+        foodLoc = make_pair(-100, -100);
         return foodLoc;
     }
 
@@ -221,10 +221,280 @@ void youWin(SDL_Renderer* renderer, SDL_Event event, int scale, int wScale, int 
 
 int main(int argc, char* argv[]){
     
-    //ToDo all...
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    if (TTF_Init() < 0){
+
+        cout << "Error: " << TTF_GetError() << endl;
+
+    }
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Event event;
 
 
+    SDL_Rect player;
+    player.x = 0;
+    player.y = 0;
+    player.h = 0;
+    player.w = 0;
 
-    cout << "Hello World" << endl;
+    int tailLength = 0;
+
+
+    vector<int> tailX;
+    vector<int> tailY;
+
+
+    int scale = 24;
+    int wScale = 24;
+
+
+    int x = 0;
+    int y = 0;
+    int prevX = 0;
+    int prevY = 0;
+
+    bool up = false;
+    bool down = false;
+    bool right = false;
+    bool left = false;
+
+    bool inputThisFrame = false;
+    bool redo = false;
+
+    SDL_Rect food;
+    food.w = scale;
+    food.h = scale;
+    food.x = 0;
+    food.y = 0;
+
+    pair<int, int> foodLoc = foodSpawn(tailX, tailY, x, y, scale, wScale, tailLength);
+    food.x = foodLoc.first;
+    food.y = foodLoc.second;
+
+    window = SDL_CreateWindow("Snake-SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, scale*wScale+1, scale*wScale+1, SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+
+    float time = SDL_GetTicks() / 100;
+
+
+    while(true){
+        float newTime = SDL_GetTicks() / 75;
+        float delta = newTime - time;
+        time = newTime;
+
+
+        inputThisFrame = false;
+
+        if(tailLength >= 575){
+            youWin(renderer, event, scale, wScale, tailLength);
+            x = 0;
+            y = 0;
+            up = false;
+            left = false;
+            right = false;
+            down = false;
+            tailX.clear();
+            tailY.clear();
+            tailLength = 0;
+            redo = false;
+            foodLoc = foodSpawn(tailX, tailY, x, y, scale, wScale, tailLength);
+
+            if(food.x == -100 && food.y == -100){
+                redo = true;
+            }
+
+            food.x = foodLoc.first;
+            food.y = foodLoc.second;
+        } 
+        
+        if (SDL_PollEvent(&event)){
+            
+            if (event.type == SDL_QUIT){
+                exit(0);
+            }
+			if (event.type == SDL_KEYDOWN && inputThisFrame == false) {
+
+				if (down == false && event.key.keysym.scancode == SDL_SCANCODE_UP) {
+					up = true;
+					left = false;
+					right = false;
+					down = false;
+					inputThisFrame = true;
+				}
+				else if (right == false && event.key.keysym.scancode == SDL_SCANCODE_LEFT) {
+					up = false;
+					left = true;
+					right = false;
+					down = false;
+					inputThisFrame = true;
+				}
+				else if (up == false && event.key.keysym.scancode == SDL_SCANCODE_DOWN) {
+					up = false;
+					left = false;
+					right = false;
+					down = true;
+					inputThisFrame = true;
+				}
+				else if (left == false && event.key.keysym.scancode == SDL_SCANCODE_RIGHT) {
+					up = false;
+					left = false;
+					right = true;
+					down = false;
+					inputThisFrame = true;
+				}
+
+			}
+    
+        }
+
+        prevX = x;
+        prevY = y;
+
+        if(up){
+            y -= delta * scale;
+        }
+
+        else if (left){
+            x -= delta * scale;
+
+        }
+        
+        else if (right){
+            x += delta * scale;
+        }
+
+        else if (down){
+            y += delta * scale;
+        }
+
+
+        if (redo == true){
+
+
+            redo = false;
+            foodLoc = foodSpawn(tailX, tailY, x, y, scale, wScale, tailLength);
+            food.x = foodLoc.first;
+            food.y = foodLoc.second;
+            
+
+            if(food.x == -100 && food.y == -100){
+                redo = true;
+            }
+        }
+
+        
+        if (checkCollision(food.x, food.y, x, y)){
+            foodLoc = foodSpawn(tailX, tailY, x, y, scale, wScale, tailLength);
+            food.x = foodLoc.first;
+            food.y = foodLoc.second;
+
+            if (food.x == -100 && food.y == -100){
+                redo = true;
+            }
+            
+            tailLength++;
+        }
+
+        if (delta * scale == 24){
+        
+            if (tailX.size() != tailLength){
+                tailX.push_back(prevX);
+                tailY.push_back(prevY);
+            }
+            
+            for (int i = 0; i < tailLength; i++){
+
+                if (i > 0){
+                    tailX[i - 1] = tailX[i];
+                    tailY[i - 1] = tailY[i];
+                }
+            }
+
+            if (tailLength > 0){
+                tailX[tailLength - 1] = prevX;
+                tailY[tailLength - 1] = prevY;
+            }
+
+
+        }
+
+        for (int i = 0; i < tailLength; i++) {
+            
+            if (x == tailX[i] && y == tailY[i]){
+                gameOver(renderer, event, scale, wScale, tailLength);
+                x = 0;
+                y = 0;
+                up = false;
+                left = false;
+                right = false;
+                down = false;
+                tailX.clear();
+                tailY.clear();
+                tailLength = 0;
+                redo = false;
+                
+                foodLoc = foodSpawn(tailX, tailY, x, y, scale, wScale, tailLength);
+                if (food.x == -100 && food.y == -100){
+                    redo = true;
+                }
+
+                
+                food.x = foodLoc.first;
+                food.y = foodLoc.second;
+
+            }
+
+        }
+        
+        if (x < 0 || y < 0 || x > scale * wScale - scale || y > scale * wScale - scale){
+
+            gameOver(renderer, event, scale, wScale, tailLength);
+            x = 0;
+            y = 0;
+            up = false;
+            left = false;
+            right = false;
+            down = false;
+            tailX.clear();
+            tailY.clear();
+            tailLength = 0;
+            redo = false;
+            foodLoc = foodSpawn(tailX, tailY, x, y, scale, wScale, tailLength);
+            food.x = foodLoc.first;
+            food.y = foodLoc.second;
+
+            if (food.x == -100 && food.y == -100){
+                redo = true;
+            }
+
+
+        }
+        
+        foodRenderer(renderer, food);
+        playerRenderer(renderer, player, x, y, scale, tailX, tailY, tailLength);
+        scoreRenderer(renderer, tailLength, scale, wScale);
+
+
+        SDL_RenderDrawLine(renderer, 0, 0, 0, 24 * 24);
+        SDL_RenderDrawLine(renderer, 0, 24*24, 24 * 24, 24 * 24);
+		SDL_RenderDrawLine(renderer, 24*24, 24 * 24, 24*24, 0);
+		SDL_RenderDrawLine(renderer, 24*24, 0, 0, 0);
+		SDL_RenderPresent(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 105, 105, 105, 255);
+		SDL_RenderClear(renderer);
+    }
+
+
+    SDL_DestroyWindow(window);
+
+    TTF_Quit();
+
+    SDL_Quit();
+
     return 0;
 }
